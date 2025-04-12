5a83a9b9c0298421386c42e648201c721252fa61
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2025-04-12 19:03:52 -0300

    Initial commit hecho hasta ahora

---
+++ devenv.nix
@@ -0,0 +1,63 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs;
+  let
+    tex = (texlive.combine {
+      inherit (texlive) scheme-small
+        wrapfig amsmath ulem hyperref capt-of
+        newverbs tikzpagenodes ifoddpage
+        dvipng minted fvextra catchfile
+        xstring framed a4wide svg trimspaces
+        transparent tocbibind microtype stix
+        geometry
+        ;
+    });
+  in [
+    git
+    emacs
+    tex
+    pandoc
+  ] ++ (if stdenv.isLinux then [
+    mininet
+  ] else []);
+
+  # https://devenv.sh/languages/
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
